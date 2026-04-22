9a70c3b45c0e82e9f5517cc67e959d66ca99f993
whypikmi <ttelatnikova1@yandex.ru>
2026-04-22 09:45:16 +0300

    Add random dice roll logic

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,56 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lab3_app/dice_roller.dart';
 
 const startAlignment = Alignment.topCenter;
 const endAlignment = Alignment.bottomCenter;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer({
-    super.key,
-    required this.color1,
-    required this.color2,
-    required this.color3,
-  });
+  const GradientContainer(this.color1, this.color2, this.color3, {super.key});
 
   final Color color1;
   final Color color2;
   final Color color3;
-
-  var activeDiceImage = 'assets/images/dice-1.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-    print('Rolling dice...');
-  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1, color2, color3],
+          colors: [
+            color1,
+            color2,
+            color3,
+          ],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 300,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: TextStyle(fontSize: 28),
-              ),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
